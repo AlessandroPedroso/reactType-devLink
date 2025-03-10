561b22b23bdd4bf0047253a6e1a79f647d1c869e
Your Name <alessanrospedroso@outlook.com>
2025-03-10 17:41:48 -0300

    Conhecendo e configurando tailwindcss

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="w-ful h-screen bg-neutral-900">
+      <h1 className="text-white text-2xl">Home</h1>
     </div>
   );
 }
