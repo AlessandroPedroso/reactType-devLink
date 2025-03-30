91a5591e9873c30ed4877abe06c6c56f5968b230
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2025-03-30 11:40:50 -0300

    configurando o login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,22 @@
 import { Link } from "react-router";
+import {Input} from '../../components/Input'
+import { FormEvent, useState } from "react";
 
 export function Login() {
+
+  const [email,setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+
+
+  function handleSubmit(e:FormEvent) {
+    e.preventDefault();
+    
+    console.log({
+      email: email,
+      password:password
+    })
+  }
+  
   return (
     <div className="flex w-full h-screen items-center justify-center flex-col">
       <Link to="/">
@@ -11,6 +27,18 @@ export function Login() {
           </span>
         </h1>
       </Link>
+
+      <form onSubmit={handleSubmit} className="w-full max-w-xl flex flex-col px-1 ">
+
+        <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Digite o seu email..." />
+        <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="**********" />
+        
+        <button type="submit" className="cursor-pointer h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white">
+          Acessar
+        </button>
+      </form>
+
+      
     </div>
   );
 }
